a808a295e13e402aef21cc0e494ec5ff25cb35a4
itskang <143569925+itskang@users.noreply.github.com>
2024-06-27 18:50:23 +0900

    Add files via upload

---
+++ ninom-html/js/custom.js
@@ -0,0 +1,16 @@
+//현재 년도 가져오기 
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+//네비게이션 메뉴 
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document
+        .querySelector(".custom_menu-btn")
+        .classList.toggle("menu_btn-style");
+}
